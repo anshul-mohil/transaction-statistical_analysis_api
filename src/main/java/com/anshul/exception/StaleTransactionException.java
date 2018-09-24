91610bfcb1b041fe97a3bdb09da2bbb5dc455efc
Anshul Mohil <anshulmohil@hotmail.com>
2018-09-24 20:16:40 +0530

    Adding business related exception

---
+++ src/main/java/com/anshul/exception/StaleTransactionException.java
@@ -0,0 +1,33 @@
+package com.anshul.exception;
+
+public class StaleTransactionException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  private String code;
+
+  private String errorMessage;
+
+  public StaleTransactionException(String code, String errorMessage) {
+    super();
+    this.code = code;
+    this.errorMessage = errorMessage;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getErrorMessage() {
+    return errorMessage;
+  }
+
+  public void setErrorMessage(String errorMessage) {
+    this.errorMessage = errorMessage;
+  }
+
+}
